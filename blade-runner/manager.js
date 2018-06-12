e9ce95de6d852d0118f7e92c050776e4fea13f52
Pedro Enrique Andrade <pefandrade@hotmail.com>
2018-06-12 00:27:28 -0300

    0.0.3

--- blade-runner/manager.js
+++ blade-runner/manager.js
@@ -1,6 +1,6 @@
 fs = require('fs');
 const SMA = require('technicalindicators').SMA;
-const pairsArray = ['DSHBTC', 'XMRBTC', 'ETHBTC'];
+const pairsArray = ['ETHBTC'];
 
 var pairs = {};
 
@@ -11,9 +11,13 @@ function Manager(){
   for(pair of pairsArray){
     pairs[pair]={
       ma: new SMA({period : maPeriods, values :[]}),
-      maValue: 0
+      maValue: 0,
+      prevMaValue: 0,
+      prevClose: 0,
+      long: false,
+      short: false,
+      stopLossPrice: 0
     }
-    console.log(pairs[pair]);
   }
 }
 
@@ -23,15 +27,81 @@ Manager.prototype.runBot = function(){
   for(pair of pairsArray){
     marketData[pair] = JSON.parse(fs.readFileSync(__dirname+'/datasets/BFX_'+pair+'_1m.json', 'utf8'));
   }
-  for( pair in marketData){
-    for(candle of marketData[pair]){
-      calculateMA(pair, candle[2])
-    }
+
+for(i=0; i<marketData[pairsArray[0]].length; i++){
+  for(pair in marketData){
+    calculateMA(pair, marketData[pair][i][2])
   }
+}
+
+//  for( pair in marketData){
+//    for(candle of marketData[pair]){
+//      calculateMA(pair, candle[2])
+//    }
+//  }
 }
 function calculateMA(pair, close){
   pairs[pair]['maValue'] = pairs[pair]['ma'].nextValue(close);
-  //console.log(pair, pairs[pair]['maValue']);
+
+  findTradeOpportunity(pair, close);
+  pairs[pair]['prevMaValue'] = pairs[pair]['maValue'];
+  pairs[pair]['prevClose'] = close;
+
+}
+
+function findTradeOpportunity(pair, close){
+  if(!pairs[pair]['long'] && !pairs[pair]['short']){
+    if(pairs[pair]['prevClose'] < pairs[pair]['prevMaValue'] &&
+      close > pairs[pair]['maValue']){
+      openLongPosition(pair, close);
+    }else if(pairs[pair]['prevClose'] > pairs[pair]['prevMaValue'] &&
+      close < pairs[pair]['maValue']){
+      openShortPosition(pair, close);
+    }
+  }else if(pairs[pair]['long']){
+    if(close < pairs[pair]['maValue']){
+      closeLongPosition(pair, close);
+    }else if(close < pairs[pair]['stopLossPrice']){
+      closeLongPosition(pair, close);
+    }
+
+  }else if(pairs[pair]['short']){
+    if(close > pairs[pair]['maValue']){
+      closeShortPosition(pair, close);
+    }else if(close > pairs[pair]['stopLossPrice']){
+      closeShortPosition(pair, close);
+    }
+  }
+}
+
+function openLongPosition(pair, close){
+  pairs[pair]['stopLossPrice'] = close * 0.98;
+  pairs[pair]['long'] = true;
+  console.log(pair, ' Opened long position at ', close);
+  console.log(pair, ' Stop loss price ', pairs[pair]['stopLossPrice']);
+  console.log('-----------------------------------------------------');
+}
+
+function openShortPosition(pair, close){
+  pairs[pair]['stopLossPrice'] = close * 1.02;
+  pairs[pair]['short'] = true;
+  console.log(pair, ' Opened short position at ', close);
+  console.log(pair, ' Stop loss price ', pairs[pair]['stopLossPrice']);
+  console.log('-----------------------------------------------------');
+}
+
+function closeLongPosition(pair, close){
+  pairs[pair]['stopLossPrice'] = 0;
+  pairs[pair]['long'] = false;
+  console.log(pair, ' Closed long position at ', close);
+  console.log('-----------------------------------------------------');
+}
+
+function closeShortPosition(pair, close){
+  pairs[pair]['stopLossPrice'] = 0;
+  pairs[pair]['short'] = false;
+  console.log(pair, ' Closed short position at ', close);
+  console.log('-----------------------------------------------------');
 }
 
 module.exports = Manager;
